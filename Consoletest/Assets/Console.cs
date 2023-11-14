ccf57bccd84fcd4196c5f8e5eb30bdb5f06fd202
Tobias Noelkensmeier <Tobiassherco125@gmail.com>
2023-11-14 22:46:56 +0200

    Made code cleaner and easier to read and understand

--- Consoletest/Assets/Console.cs
+++ Consoletest/Assets/Console.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -14,26 +12,31 @@ public class Console : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Input to open console.
+        //Eingabe um Konsole zu öffnen.
         if (Input.GetKeyDown(KeyCode.BackQuote))
         {
           toggleConsole();
         }
-        
-        if (Input.GetKeyDown(KeyCode.Return))
-            {
-                command = inputField.text;
-                inputField.text = "";
-            }
         void toggleConsole()
         {
             bool currentstate = console.activeSelf;
             console.SetActive(!currentstate);
         }
-
-
-
+        
+        //Input to send command.
+        //Eingabe um Befehl zu senden.
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            command = inputField.text;
+            inputField.text = "";
+        }
+        
+        //Example use case: Loading a level via Command.
+        //Beischpiel für die Verwendung laden eines Levels über Befehl.
         switch (command)
         {
+            //Command Name: LoadLevel_2
             case "LoadLevel_2":
                 SceneManager.LoadScene("Scene2");
                 break;
