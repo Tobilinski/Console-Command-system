1771bdd4621eecae14a00cd14bbc316bf744875f
Tobias Noelkensmeier <Tobiassherco125@gmail.com>
2023-12-11 14:44:28 +0200

    Added new Feature
    New feature: Password functionality so that only the developer can use the console
    
    Getting ready for publishing

---
+++ Consoletest/Assets/Tobilinski Code Tools/Console.cs
@@ -0,0 +1,164 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+// This script is used to create a console for the game.
+// Dieses Skript wird verwendet um eine Konsole für das Spiel zu erstellen.
+
+// The console can be opened by pressing the backquote key.
+// Die Konsole kann geöffnet werden indem die Backquote Taste gedrückt wird.
+
+// The console can be closed by pressing the backquote key again.
+// Die Konsole kann geschlossen werden indem die Backquote Taste erneut gedrückt wird.
+
+// Requirements:
+    // Canvas with an empty GameObject, InputField and a Text element.
+    // Empty GameObject in the Canvas must be named "ConsoleParent".
+    // InputField in UI must be named "Console".
+    // Text element in UI must be named "Output".
+    // Attach teh script anywhere in the scene.
+
+// Anforderungen:
+    // Canvas mit einem leeren GameObject, InputField und einem Text Element.
+    // Leeres GameObject im Canvas muss "ConsoleParent" heißen.
+    // InputField im UI muss "Console" heißen.
+    // Text Element im UI muss "Output" heißen.
+    // Script irgendwo in der Szene anhängen.
+public class Console : MonoBehaviour
+{
+    private InputField _inputField;
+    private Text _consoleText;
+    private GameObject _ConsoleParent;
+    
+    private InputField _inputFieldPassword;
+    private Text _consoleTextPassword;
+    private GameObject _PasswordParent;
+    
+    [SerializeField]
+    private string _password;
+    static List<string> typedCommands = new List<string>();
+    private void Start()
+    {
+        // Finding and assigning references to UI elements.
+        // Finden von Gameobject fuer die Konsole der Referenze.
+        _consoleText = GameObject.Find("Output").GetComponent<Text>();
+        _inputField = GameObject.Find("Console").GetComponent<InputField>();
+        _ConsoleParent = GameObject.Find("ConsoleParent");
+        
+        _PasswordParent = GameObject.Find("PasswordParent");
+        _inputFieldPassword = GameObject.Find("PasswordInput").GetComponent<InputField>();
+        _consoleTextPassword = GameObject.Find("OutputPass").GetComponent<Text>();
+        
+        // Setting the console UI to be inactive by default.
+        // Setzen der Konsole auf inaktiv als Standard.
+        _PasswordParent.SetActive(false);
+        _ConsoleParent.SetActive(false);
+        
+        // Displaying previously typed commands in the console.
+        // Anzeigen von zuvor eingegebenen Befehlen in der Konsole.
+        for (int i = 0; i < typedCommands.Count; i++)
+        {
+            _consoleText.text += "\n" + typedCommands[i];
+        }
+        for (int i = 0; i < typedCommands.Count; i++)
+        {
+            _consoleTextPassword.text += "\n" + typedCommands[i];
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //Input to open console.
+        //Eingabe um Konsole zu öffnen.
+        if (Input.GetKeyDown(KeyCode.BackQuote))
+        {
+           if(_ConsoleParent.activeSelf)
+            {
+                toggleConsole();
+            }
+            else
+            {
+                togglePassword();
+            }
+        }
+
+       
+        
+        //Input to send command.
+        //Eingabe um Befehl zu senden.
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            string command = _inputField.text;
+            //Adding the command to the list of typed commands.
+            //Hinzufügen des Befehls zur Liste der eingegebenen Befehle.
+            typedCommands.Add(command);
+            //Only adding the command to the consoleText (Display) if it is not empty.
+            //Nur hinzufügen des Befehls zum consoleText (Anzeige) wenn er nicht leer ist.
+            if (!string.IsNullOrEmpty(command))
+            {
+                _consoleText.text += "\n" + command;
+                _inputField.text = "";
+            }
+
+            
+            //Example use case: Loading a level via Command.
+            //Beischpiel für die Verwendung von laden eines Levels über Befehl.
+            switch (command)
+            {
+                //Command Name: LoadLevel_1
+                case "LoadLevel_1":
+                    SceneManager.LoadScene("test");
+                    break;
+                //Command Name: LoadLevel_2
+                case "LoadLevel_2":
+                    SceneManager.LoadScene("Scene2");
+                    break;
+                //Command Name: LoadLevel_3
+                case "LoadLevel_3":
+                    SceneManager.LoadScene("Scene3");
+                    break;
+            }
+        }
+        //Input to send password to access console.
+        //Eingabe um Passwort zu senden um Konsole zu öffnen.
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            string command = _inputFieldPassword.text;
+
+            //Adding the command to the list of typed commands.
+            //Hinzufügen des Befehls zur Liste der eingegebenen Befehle.
+            typedCommands.Add(command);
+            //Only adding the command to the consoleText (Display) if it is not empty.
+            //Nur hinzufügen des Befehls zum consoleText (Anzeige) wenn er nicht leer ist.
+            if (!string.IsNullOrEmpty(command))
+            {
+                _consoleTextPassword.text += "\n" + command;
+                _inputFieldPassword.text = "";
+            }
+            
+            //Checking if the passwas is correct and opening the console if it is.
+            //Überprüfen ob das Passwort korrekt ist und öffnen der Konsole wenn es korrekt ist.
+            if (command == _password)
+            {
+                togglePassword();
+                toggleConsole();
+            }
+            else
+            {
+                _consoleTextPassword.text += "\n" + "Wrong Password";
+            }
+        }
+        void toggleConsole()
+        {
+            bool currentstateConsole = _ConsoleParent.activeSelf;
+            _ConsoleParent.SetActive(!currentstateConsole);
+        }
+        void togglePassword()
+        {
+            bool currentstatePassword = _PasswordParent.activeSelf;
+            _PasswordParent.SetActive(!currentstatePassword);
+        }
+    }
+}
